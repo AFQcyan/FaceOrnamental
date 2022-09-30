b70b2b80d990ff6a0d74c565f6b738b78324163b
AFQcyan <91124299+AFQcyan@users.noreply.github.com>
2022-09-30 12:56:33 +0900

    얼굴 분석 정보 전부 출력 / 사진 분석

--- src/main/java/com/example/faceornamental/FaceDefrag.java
+++ src/main/java/com/example/faceornamental/FaceDefrag.java
@@ -101,15 +101,17 @@ public class FaceDefrag{
             e.printStackTrace();
         }
 
-        //books의 배열을 추출
+        //faces의 배열을 추출
         JSONArray getFaces = (JSONArray) jsonObject.get("faces");
+        int faceLength = getFaces.size();
+        System.out.println(faceLength);
         JSONObject getFaceInfo = (JSONObject) getFaces.get(0);
 
         JSONObject getEmotion = (JSONObject) getFaceInfo.get("emotion");
-        String Emotion = (String) getEmotion.get("value");
+        String emotion = (String) getEmotion.get("value");
         Double getEmotionConfidence = (Double) getEmotion.get("confidence");
         Double emotionConfidence = (double)Math.round(getEmotionConfidence * 1000000)/10000;
-        System.out.println("감정 : " + Emotion + " / 확률 : " + emotionConfidence + "%");
+        System.out.println("감정 : " + emotion + " / 확률 : " + emotionConfidence + "%");
 
         JSONObject getGender = (JSONObject) getFaceInfo.get("gender");
         String gender = (String) getGender.get("value");
@@ -128,5 +130,56 @@ public class FaceDefrag{
         Double getPoseConfidence = (Double) getPose.get("confidence");
         Double poseConfidence =  (double)Math.round(getPoseConfidence * 1000000)/10000;
         System.out.println("자세 : " + pose + " / 확률 : " + poseConfidence + "%");
+
+        //얼굴좌표 불러오기
+
+        JSONObject getFaceLocate = (JSONObject) getFaceInfo.get("roi");
+        long faceStartX = (long) getFaceLocate.get("x");
+        long faceStartY = (long) getFaceLocate.get("y");
+        long faceWidth = (long) getFaceLocate.get("width");
+        long faceHeight = (long) getFaceLocate.get("height");
+        System.out.println("얼굴 시작 좌표 : ( " + faceStartX+ " , " + faceStartY + " )");
+        System.out.println("얼굴 넓이 : ( " + faceWidth+ " , " + faceHeight + " )");
+        long faceEndX = faceStartX + faceWidth;
+        long faceEndY = faceStartY + faceHeight;
+
+        JSONObject getElementLocate = (JSONObject) getFaceInfo.get("landmark");
+
+        JSONObject getLeftEye = (JSONObject) getElementLocate.get("leftEye");
+        long leftEyeX = (long) getLeftEye.get("x");
+        long leftEyeY = (long) getLeftEye.get("y");
+        long relativeLeftEyeX = leftEyeX - faceStartX;
+        long relativeLeftEyeY = leftEyeY - faceStartY;
+        System.out.println("왼쪽 눈 상대좌표 : ( " + relativeLeftEyeX + " , " + relativeLeftEyeY + " )");
+
+        JSONObject getRightEye = (JSONObject) getElementLocate.get("rightEye");
+        long rightEyeX = (long) getRightEye.get("x");
+        long rightEyeY = (long) getRightEye.get("y");
+        long relativeRightEyeX = rightEyeX - faceStartX;
+        long relativeRightEyeY = rightEyeY - faceStartY;
+        System.out.println("오른쪽 눈 상대좌표 : ( " + relativeRightEyeX + " , " + relativeRightEyeY + " )");
+
+        JSONObject getNose = (JSONObject) getElementLocate.get("nose");
+        long noseX = (long) getNose.get("x");
+        long noseY = (long) getNose.get("y");
+        long relativeNoseX = noseX - faceStartX;
+        long relativeNoseY = noseY - faceStartY;
+        System.out.println("코 상대좌표 : ( " + relativeNoseX + " , " + relativeNoseY + " )");
+
+        JSONObject getLeftMouth = (JSONObject) getElementLocate.get("leftMouth");
+        long leftMouthX = (long) getLeftMouth.get("x");
+        long leftMouthY = (long) getLeftMouth.get("y");
+        long relativeLeftMouthX = leftMouthX - faceStartX;
+        long relativeLeftMouthY = leftMouthY - faceStartY;
+        System.out.println("왼쪽 입 상대좌표 : ( " + relativeLeftMouthX + " , " + relativeLeftMouthY + " )");
+
+        JSONObject getRightMouth = (JSONObject) getElementLocate.get("rightMouth");
+        long rightMouthX = (long) getRightMouth.get("x");
+        long rightMouthY = (long) getRightMouth.get("y");
+        long relativeRightMouthX = rightMouthX - faceStartX;
+        long relativeRightMouthY = rightMouthY - faceStartY;
+        System.out.println("오른쪽 입 상대좌표 : ( " + relativeRightMouthX + " , " + relativeRightMouthY + " )");
+
+
     }
 }
